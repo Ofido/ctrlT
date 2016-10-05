03ecdc315dc24a7593175ac6f773cca6076919e8
Ofido <danieldsb98@gmail.com>
2016-10-05 10:41:44 -0300

    as

--- src/br/com/doors/ctrlt/controller/EspecialistaController.java
+++ src/br/com/doors/ctrlt/controller/EspecialistaController.java
@@ -1,18 +1,60 @@
 package br.com.doors.ctrlt.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.doors.ctrlt.dao.EspecialistaDAO;
+import br.com.doors.ctrlt.model.Especialista;
 
 @Controller
 public class EspecialistaController {
-	private final EspecialistaDAO dao;
+	private final EspecialistaDAO espDAO;
 	
 	@Autowired
 	public EspecialistaController(EspecialistaDAO daoE){
-		this.dao = daoE;
+		this.espDAO = daoE;
+	}
+	
+	@RequestMapping("CadastrandoEspecialista")
+	public String caminhoCadastro(HttpSession session, Long id) {
+		if (id != null) {			
+			session.setAttribute("alterando", espDAO.procurar(id));
+		}
+		return "CadastroEspecialista";
+	}
+	
+	@RequestMapping("ListandoEspecialista")
+	private String listar(HttpSession session) {
+		session.setAttribute("lista", espDAO.listarTodos());
+		return "ListarEspecialista";
+	}
+	@RequestMapping("ExcluirEspecialista")
+	private String excluir(Long id) {
+		espDAO.excluir(id);
+		return "redirect:ListandoEspecialista";
+	}
+	
+	@RequestMapping("CadastroEspecialista")
+	public String cadastro(HttpSession session, Especialista especialista, MultipartFile arquivo, Boolean licenca) {
+		if (!arquivo.isEmpty()) {
+			try {
+				especialista.setFotoEspecialista(arquivo.getBytes());
+			} catch (IOException e) {
+				throw new RuntimeException("ERRO NA FOTO"+ this.getClass() + e.toString());
+			}
+		}
+		if (especialista.getIdEspecialista() == null) {			
+			espDAO.incluir(especialista);
+		}else {
+			espDAO.alterar(especialista);
+		}
+		return "Index";
 	}
 	
 	@RequestMapping("voltaInicioEspecialista")
--- src/br/com/doors/ctrlt/dao/EspecialistaDAO.java
+++ src/br/com/doors/ctrlt/dao/EspecialistaDAO.java
@@ -1,9 +1,11 @@
 package br.com.doors.ctrlt.dao;
 
+import java.io.ByteArrayInputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -16,6 +18,11 @@ import br.com.doors.ctrlt.model.Especialista;
 @Repository
 public class EspecialistaDAO implements InterfaceEspecialistaDAO {
 	private static final String LOGIN = "SELECT * FROM especialista WHERE emailespecialista=? AND senhaespecialista=?";
+	private static final String INCLUIR = "insert into ctrlt.especialista(cpfEspecialista,senhaEspecialista,nomeEspecialista,emailEspecialista,telefoneEspecialista,fotoEspecialista) value (?,?,?,?,?,?)";
+	private static final String EXCLUIR = "delete from ctrlt.especialista where idespecialista=?";
+	private static final String ALTERAR = "update ctrlt.especialista set cpfEspecialista=?,senhaEspecialista=?,nomeEspecialista=?,emailEspecialista=?,telefoneEspecialista=?,fotoEspecialista=?";
+	private static final String LISTAR = "select * from ctrlt.especialista";
+	private static final String PROCURAR = "select * from ctrlt.especialista where idespecialista=?";
 
 	//CONEXÃO
 	private static Connection CONEXAO;
@@ -31,20 +38,82 @@ public class EspecialistaDAO implements InterfaceEspecialistaDAO {
 
 	@Override
 	public void incluir(Especialista t) {
-		// TODO Auto-generated method stub
-		
+		if (t==null) {
+			throw new RuntimeException("ESPECIALISTA NÃO PODE SER NULO" + this.getClass());
+		}
+		try {
+			PreparedStatement stmt = CONEXAO.prepareStatement(INCLUIR, PreparedStatement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, t.getCpfEspecialista());
+			stmt.setString(2, t.getSenhaEspecialista());
+			stmt.setString(3, t.getNomeEspecialista());
+			stmt.setString(4, t.getEmailEspecialista());
+			stmt.setString(5, t.getTelefoneEspecialista());
+			stmt.setBlob(6, new ByteArrayInputStream(t.getFotoEspecialista()));
+			stmt.execute();
+			ResultSet rs = stmt.getGeneratedKeys();
+			if(rs.next()){
+				t.setIdEspecialista(rs.getLong(1));
+			}
+			rs.close();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException("ERRO NO CADASTRO DE Especialista\n" + this.getClass() +"\n"+ e.toString());
+		}
 	}
 
 	@Override
 	public void alterar(Especialista t) {
-		// TODO Auto-generated method stub
-		
+		if (t==null) {
+			throw new RuntimeException("ESPECIALISTA NÃO PODE SER NULO" + this.getClass());
+		}
+		Especialista especialista = procurar(t.getIdEspecialista());
+		if (t.getCpfEspecialista() == null || t.getCpfEspecialista().isEmpty()) {
+			t.setCpfEspecialista(especialista.getCpfEspecialista());
+		}
+		if (t.getSenhaEspecialista() == null || t.getSenhaEspecialista().isEmpty()) {
+			t.setSenhaEspecialista(especialista.getSenhaEspecialista());
+		}
+		if (t.getNomeEspecialista() == null || t.getNomeEspecialista().isEmpty()) {
+			t.setNomeEspecialista(especialista.getNomeEspecialista());
+		}
+		if (t.getEmailEspecialista() == null || t.getEmailEspecialista().isEmpty()) {
+			t.setEmailEspecialista(especialista.getEmailEspecialista());
+		}
+		if (t.getTelefoneEspecialista() == null || t.getTelefoneEspecialista().isEmpty()) {
+			t.setTelefoneEspecialista(especialista.getTelefoneEspecialista());
+		}
+		if (t.getFotoEspecialista() == null || t.getFoto64().isEmpty()) {
+			t.setFotoEspecialista(especialista.getFotoEspecialista());
+		}
+		try {
+			PreparedStatement stmt = CONEXAO.prepareStatement(ALTERAR);
+			stmt.setString(1, t.getCpfEspecialista());
+			stmt.setString(2, t.getSenhaEspecialista());
+			stmt.setString(3, t.getNomeEspecialista());
+			stmt.setString(4, t.getEmailEspecialista());
+			stmt.setString(5, t.getTelefoneEspecialista());
+			stmt.setBlob(6, new ByteArrayInputStream(t.getFotoEspecialista()));
+			stmt.setLong(7, t.getIdEspecialista());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException("ERRO NA ALTERACAO DE Especialista\n" + this.getClass() +"\n"+ e.toString());
+		}
 	}
 
 	@Override
 	public void excluir(Long id) {
-		// TODO Auto-generated method stub
-		
+		if (id==null) {
+			throw new RuntimeException("O CÓDIGO NÃO PODE SER NULO" + this.getClass());
+		}
+		try {
+			PreparedStatement stmt = CONEXAO.prepareStatement(EXCLUIR);
+			stmt.setLong(1, id);
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException("ERRO NA EXCLUSÃO DE ESPECIALISTA" + this.getClass() + e.toString());
+		}
 	}
 
 	@Override
@@ -55,8 +124,29 @@ public class EspecialistaDAO implements InterfaceEspecialistaDAO {
 
 	@Override
 	public List<Especialista> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Especialista> especialistas = new ArrayList<Especialista>();
+		try {
+			PreparedStatement stmt = CONEXAO.prepareStatement(LISTAR);
+
+			ResultSet rs = stmt.executeQuery();
+			Especialista especialista;
+			while (rs.next()) {
+				especialista = new Especialista();
+				especialista.setIdEspecialista(rs.getLong("idprofessor"));
+				especialista.setNomeEspecialista(rs.getString("nomeprofessor"));
+				especialista.setEmailEspecialista(rs.getString("emailprofessor"));
+				especialista.setSenhaEspecialista(rs.getString("senhaprofessor"));
+				especialista.setCpfEspecialista(rs.getString("cpfProfessor"));
+				especialista.setTelefoneEspecialista(rs.getString("telefoneProfessor"));
+				especialista.setFotoEspecialista(rs.getBytes("fotoProfessor"));
+				especialistas.add(especialista);
+			}
+			rs.close();
+			stmt.close();
+			return especialistas;
+		} catch (SQLException e) {
+			throw new RuntimeException("ERRO NO LISTAR DE ESPECIALISTA" + this.getClass() + e.toString());
+		}
 	}
 
 	@Override
@@ -101,7 +191,33 @@ public class EspecialistaDAO implements InterfaceEspecialistaDAO {
 
 	@Override
 	public Especialista procurar(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		if (id==null) {
+			throw new RuntimeException("O CODIGO NAO PDE SER NULO" + this.getClass());
+		}
+		try {
+			PreparedStatement stmt = CONEXAO.prepareStatement(PROCURAR);
+
+			stmt.setLong(1, id);
+
+			ResultSet rs = stmt.executeQuery();
+			Especialista especialista;
+			if (rs.next()) {
+				especialista = new Especialista();
+				especialista.setIdEspecialista(rs.getLong("idespecialista"));
+				especialista.setNomeEspecialista(rs.getString("nomeespecialista"));
+				especialista.setEmailEspecialista(rs.getString("emailespecialista"));
+				especialista.setSenhaEspecialista(rs.getString("senhaespecialista"));
+				especialista.setCpfEspecialista(rs.getString("cpfespecialista"));
+				especialista.setTelefoneEspecialista(rs.getString("telefoneespecialista"));
+				especialista.setFotoEspecialista(rs.getBytes("fotoespecialista"));
+			} else {
+				especialista = null;
+			}
+			rs.close();
+			stmt.close();
+			return especialista;
+		} catch (SQLException e) {
+			throw new RuntimeException("ERRO NO PROCURAR DE ESPECIALISTA" + this.getClass() + e.toString());
+		}
 	}
 }

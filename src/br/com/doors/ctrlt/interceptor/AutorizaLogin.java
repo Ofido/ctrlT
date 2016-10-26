074daa6dcd1c484f8bcf6f6cfbebd9ac816991a5
danie_000 <danie_000@192.168.1.39>
2016-10-26 18:09:07 -0200

    att

--- src/br/com/doors/ctrlt/interceptor/AutorizaLogin.java
+++ src/br/com/doors/ctrlt/interceptor/AutorizaLogin.java
@@ -10,12 +10,17 @@ public class AutorizaLogin extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object controller) throws Exception {
-		
+
 		String uri = request.getRequestURI();
-		if(uri.endsWith("Login") ||uri.endsWith("efetuaLogin") || uri.contains("resources") || uri.contains("CadastrandoAluno") || uri.contains("CadastrandoProfessor")){
+		if (uri.endsWith("Login") || uri.endsWith("efetuaLogin")
+				|| uri.contains("resources")
+				|| uri.contains("CadastrandoAluno")
+				|| uri.contains("CadastroAluno")
+				|| uri.contains("CadastrandoProfessor")
+				|| uri.contains("CadastroProfessor")) {
 			return true;
 		}
-		if(request.getSession().getAttribute("usuarioLogado") != null){
+		if (request.getSession().getAttribute("usuarioLogado") != null) {
 			return true;
 		}
 		response.sendRedirect("views/Login.jsp");
